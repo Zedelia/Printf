a97f7479fd933b867b108abdb6f7bde7a454433d
Melodie Bos <mbos@z1r1p4.le-101.fr>
2019-11-25 19:23:03 +0100

    add_add_pattern

--- srcs/format_parser.c
+++ srcs/format_parser.c
@@ -6,35 +6,47 @@
 /*   By: mbos <marvin@le-101.fr>                    +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/18 18:55:10 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/25 18:33:05 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/25 19:22:31 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
+void format_add_pattern(t_format *s_format, t_pattern *l_pattern)
+{
+	t_pattern *temp;
+
+	temp = s_format->l_pattern;
+	while (temp && temp->next)
+		temp = temp->next;
+	temp->next = l_pattern;
+}
+
+
+
+
+
 t_bool	format_parser(t_format *s_format, char *format, va_list params)
 {
-	int i;
 	t_pattern *l_pattern;
-	char *cp_format;
+	char 	*cp_format;
 
-	i = 0;
-	if ((cp_format = ft_strchr(format, "%")
-	{
-		if (!(pattern_init(&l_pattern, cp_format, params)))
-			return (False);
-		s_format->l_pattern = l_pattern;
-		while (cp_format[i])
+	cp_format = format;
+	// if ((cp_format = ft_strchr(cp_format, "%")
+	// {
+	// 	if (!(pattern_init(&l_pattern, cp_format, params)))
+	// 		return (False);
+	// 	s_format->l_pattern = l_pattern;
+
+// methode pour ce morceau
+		while (cp_format && (cp_format = ft_strchr(cp_format + 1, "%")))
 		{
-			if (cp_format == '%')
-			{
-				if (!(pattern_init(&(l_pattern->next), &cp_format[i], params)))
+			if (!(pattern_init(&l_pattern, cp_format, params)))
 					return (False);
-				l_pattern = l_pattern->next;
-			}
-			i++;
+			format_add_pattern(s_format, l_pattern);
 		}
-	}
+
+	// }
 	return (True);
 }
