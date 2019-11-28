72d71718220c14e3d6a1dc0c60d0b574ba190fef
Zedelia <mbos@z1r4p3.le-101.fr>
2019-11-28 17:46:00 +0100

    make_flag_init_fct

--- srcs/flag_init.c
+++ srcs/flag_init.c
@@ -6,15 +6,20 @@
 /*   By: mbos <marvin@le-101.fr>                    +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/25 17:36:41 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/25 19:24:40 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/28 17:11:59 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-t_bool	flags_init(t_flag **l_flag, char *format, va_list params)
+t_bool	flag_init(t_flag **l_flag, char *flag, va_list params)
 {
-	(*flag)->next = NULL;
-	flag_parser(l_flag, format)
+	(void)params;
+	if (!(*l_flag = malloc(sizeof(t_flag))))
+		return (False);
+	(*l_flag)->flag_type = flag[0];
+	(*l_flag)->next = NULL;
+	// flag_parser(*l_flag, params);
+	return (True);
 }
