7ac0f6aa71ff401d0c0ad1ce58e6e2ea43604640
Zedelia <mbos@z1r4p3.le-101.fr>
2019-11-28 17:43:43 +0100

    delete_useless_comments

--- srcs/format_parser.c
+++ srcs/format_parser.c
@@ -6,7 +6,7 @@
 /*   By: mbos <marvin@le-101.fr>                    +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/18 18:55:10 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/28 13:33:36 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/28 16:48:45 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -36,20 +36,20 @@ t_bool	format_parser(t_format *s_format, char *format, va_list params)
 	char 	   *cp_format;
 
 	cp_format = format;
-// probleme quand format commence par %
 	if (format[0] == '%')
 	{
 		if (!(pattern_init(&l_pattern, cp_format, params)))
 			return (False);
 		format_add_pattern(s_format, l_pattern);
 	}
-// methode pour ce morceau ?
 	while (cp_format && (cp_format = ft_strchr(cp_format + 1, '%')))
 	{
 		if (!(pattern_init(&l_pattern, cp_format, params)))
 			return (False);
 		format_add_pattern(s_format, l_pattern);
 	}
+
+	//ca ce sont des tests : a supprimer
 	show_pattern(s_format->l_pattern);
 	show_format(s_format);
 
