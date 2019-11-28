62d8033e36dc757a51a7fec516139b3178e6ce86
Zedelia <mbos@z1r4p3.le-101.fr>
2019-11-28 17:39:46 +0100

    function_get_indicator

---
+++ srcs/get_indicator.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   get_indicator.c                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mbos <marvin@le-101.fr>                    +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/11/28 17:17:42 by mbos         #+#   ##    ##    #+#       */
+/*   Updated: 2019/11/28 17:33:43 by mbos        ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+char 	get_indicator(char *pattern)
+{
+	int i;
+	int j;
+
+	i = 1;
+	j = 0;
+	while (pattern[i])
+	{
+		while (INDICATORS[j])
+		{
+			if (pattern[i] == INDICATORS[j])
+			{
+				printf("indic = %c\n", pattern[i]);
+				return (pattern[i]);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (False);
+}
