b4dccbe32b77026730100929c7a93ab9e9b877e7
Zedelia <mbos@z1r4p3.le-101.fr>
2019-11-28 17:47:13 +0100

    init_pattern_parsing_with_flag

--- srcs/pattern_parser.c
+++ srcs/pattern_parser.c
@@ -6,31 +6,48 @@
 /*   By: mbos <marvin@le-101.fr>                    +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/27 10:05:10 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/28 12:50:15 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/28 17:40:57 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
+void 	pattern_add_flag(t_pattern *l_pattern, t_flag *l_flag)
+{
+	t_flag *temp;
+
+	temp = l_pattern->l_flag;
+	if (temp)
+	{
+		while (temp->next)
+			temp = temp->next;
+		temp->next = l_flag;
+	}
+	else
+		l_pattern->l_flag = l_flag;
+}
+
+
 t_bool 	pattern_parser(t_pattern *l_pattern, va_list params)
 {
-	// t_flag *l_flags;
+	t_flag *l_flag;
 
 	if (ft_is_indicateur(l_pattern->pattern[1]) == True)
 	{
 		l_pattern->indicateur = l_pattern->pattern[1];
 		l_pattern->l_flag = NULL;
-// a supprimer cette ligne, on garde pour tests
 		get_arg(l_pattern, params);
 		l_pattern->len = 2;
 		return (True);
 	}
-	// else if (ft_is_indicateur(l_pattern->pattern[1]) == False)
-	// {
-	// 	if (!(flags_init(&l_flags, &(l_pattern->pattern[1]), params)))
-	// 		return (False);
-	// 	l_pattern->l_flag = l_flags;
-	// }
+	else if (ft_is_indicateur(l_pattern->pattern[1]) == False)
+	{
+		if (!(flag_init(&l_flag, &(l_pattern->pattern[1]), params)))
+			return (False);
+		pattern_add_flag(l_pattern, l_flag);
+		l_pattern->indicateur = get_indicator(l_pattern->pattern);
+		get_arg(l_pattern, params);
+	}
 	return(True);
 }
