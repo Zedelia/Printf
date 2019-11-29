e4032e1dbb324a17e24ee017e3f5180ce111d86f
Zedelia <melodiebos@gmail.com>
2019-11-29 17:54:08 +0100

    mute_flag_free_one_noMoreNextInFlags

--- srcs/flag_free.c
+++ srcs/flag_free.c
@@ -6,30 +6,40 @@
 /*   By: mbos <marvin@le-101.fr>                    +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/25 19:29:25 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/28 17:01:35 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/29 17:52:58 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-void 	flag_free_one(t_flag **l_flag)
+void 	flag_free(t_flag **l_flag)
 {
 	if (!l_flag)
 		return ;
+	if ((*l_flag)->precision)
+	{
+		ft_memdel((void**)&((*l_flag)->precision));
+		(*l_flag)->precision = NULL;
+	}
+	if ((*l_flag)->width)
+	{
+		ft_memdel((void**)&((*l_flag)->width));
+		(*l_flag)->precision = NULL;
+	}
 	ft_memdel((void**) l_flag);
 	l_flag = NULL;
 }
 
-void 	flag_free(t_flag **l_flag)
-{
-	t_flag *temp;
-
-	while (*l_flag)
-	{
-		temp = (*l_flag)->next;
-		flag_free_one(l_flag);
-		*l_flag = temp;
-	}
-	l_flag = NULL;
-}
+// void 	flag_free(t_flag **l_flag)
+// {
+// 	t_flag *temp;
+//
+// 	while (*l_flag)
+// 	{
+// 		temp = (*l_flag)->next;
+// 		flag_free_one(l_flag);
+// 		*l_flag = temp;
+// 	}
+// 	l_flag = NULL;
+// }
