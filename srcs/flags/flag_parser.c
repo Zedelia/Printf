d45a8b72684d5697b3e454d6966f752b3ad71816
Zedelia <melodiebos@gmail.com>
2019-12-02 19:30:55 +0100

    fix_get_width

--- srcs/flags/flag_parser.c
+++ srcs/flags/flag_parser.c
@@ -6,7 +6,7 @@
 /*   By: mbos <marvin@le-101.fr>                    +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/29 12:03:18 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/02 15:19:15 by melodiebos  ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/02 19:28:47 by melodiebos  ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,7 +18,7 @@ t_bool		flag_parser(t_flag *l_flag, char *flags, va_list params)
 	if (flags[0] == '0' || flags[0] == '-')
 	{
 		l_flag->flag_type = flags[0];
-		if (!(flag_width(l_flag, flags, params)))
+		if (!(flag_width(l_flag, &flags[1], params)))
 			return (false_ret(__func__));
 		return (True);
 	}
