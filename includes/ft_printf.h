5e01eaa2afd7762245625f338fef04f5bbdd5fbd
Zedelia <melodiebos@gmail.com>
2019-12-17 16:22:24 +0100

    update_headers

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mbos <marvin@le-101.fr>                    +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/15 11:58:47 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/17 16:19:29 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/17 16:21:17 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -35,19 +35,6 @@
 # include "format.h"
 # include "get.h"
 # include "tests.h"
-
-/*
-** Utils functions
-*/
-int			put_output(const char *format, t_format *s_format, int ret);
-int			ft_printf(const char *s, ...) __attribute__((format(printf, 1, 2)));
-
-size_t		ft_index(char c, const char *charset);
-int			is_flags(char c);
-t_bool		is_indicator(char c);
-int			occur_before(char c, char *charset, char before);
-int			occur_after(char c, char *charset, char after);
-char		*create_malloc(char *cpy_output, int len);
-char		*ft_utoa(unsigned int nbr);
+# include "utils.c"
 
 #endif
