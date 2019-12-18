c5c1b698d5097c6e63a85f5b2cf6997ed1e44d63
Zedelia <melodiebos@gmail.com>
2019-12-18 15:54:45 +0100

    some_cleaning

--- srcs/tests/tests_utils.c
+++ srcs/tests/tests_utils.c
@@ -3,18 +3,17 @@
 /*                                                              /             */
 /*   tests_utils.c                                    .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: mbos <marvin@le-101.fr>                    +:+   +:    +:    +:+     */
+/*   By: mbos <mbos@student.le-101.fr>              +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/30 12:36:10 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/30 16:08:40 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/18 15:42:36 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../../includes/ft_printf.h"
 
-// false_ret(__func__)
-t_bool false_ret(const char *namefunc)
+t_bool	false_ret(const char *namefunc)
 {
 	printf(RED"%s"RESET"\n", namefunc);
 	return (False);
