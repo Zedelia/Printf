c9df0ea7050d7185c2ef50b099008c63922bb287
Zedelia <melodiebos@gmail.com>
2019-12-18 15:37:44 +0100

    norm_format

--- srcs/format/format_free.c
+++ srcs/format/format_free.c
@@ -3,20 +3,20 @@
 /*                                                              /             */
 /*   format_free.c                                    .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: mbos <marvin@le-101.fr>                    +:+   +:    +:    +:+     */
+/*   By: mbos <mbos@student.le-101.fr>              +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/15 12:02:30 by mbos         #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/17 16:11:57 by mbos        ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/18 15:37:11 by mbos        ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../../includes/ft_printf.h"
 
-void format_free(t_format **s_format)
+void	format_free(t_format **s_format)
 {
 	ft_memdel((void **)&(*s_format)->format);
 	(*s_format)->pointer_f = NULL;
 	input_free(&((*s_format)->l_input));
-	ft_memdel((void**) s_format);
+	ft_memdel((void**)s_format);
 }
